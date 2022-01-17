13974f366c24ad30b5d05fd891a39cd7409cb200
joaquin <jecorrea@protonmail.com>
2022-01-16 21:40:05 -0500

    initial JS

---
+++ script.js
@@ -0,0 +1,32 @@
+// Bring down the elements and dates
+
+const hourEl = document.querySelector(".hour");
+const minuteEl = document.querySelector(".minute");
+const secondEl = document.querySelector(".second");
+const timeEl = document.querySelector(".time");
+const dateEl = document.querySelector(".date");
+const toggle = document.querySelector(".toggle");
+
+const days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+const months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
