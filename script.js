d1c34e5f5ba12234266a16a983dea5c7aed31ca2
joaquin <jecorrea@protonmail.com>
2022-01-16 21:48:14 -0500

    added functionality for dark mode

--- script.js
+++ script.js
@@ -30,3 +30,16 @@ const months = [
   "Nov",
   "Dec",
 ];
+
+// Button functionality
+
+toggle.addEventListener("click", (e) => {
+  const html = document.querySelector("html");
+  if (html.classList.contains("dark")) {
+    html.classList.remove("dark");
+    e.target.innerHTML = "Dark mode";
+  } else {
+    html.classList.add("dark");
+    e.target.innerHTML = "Light mode";
+  }
+});
